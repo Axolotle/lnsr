2557ce1945e5465c7a54f217bdb102d0c54876d2
Axolotle <nchesnais@mailoo.org>
2018-11-20 18:27:19 +0100

    add fixed background generator

---
+++ generators/background.py
@@ -0,0 +1,53 @@
+from json import loads
+from random import randint, sample
+from svgwrite import Drawing
+from svgwrite.shapes import Line
+from svgwrite.container import Group
+
+class BackgroundGenerator:
+    def __init__(self):
+        with open('generators/backgroundData.json', 'r') as data:
+            data = loads(data.read())
+            self.width = data['width']
+            self.height = data['height']
+            self.style = data['style']
+            self.verticalInterval = data['verticalInterval']
+
+        self.verticalLines = round(self.height / self.verticalInterval)
+        self.viewBox = '0 0 {} {}'.format(self.width, self.height)
+
+    def generate_file(self):
+        document = self.getSVGDocument()
+        elements = self.getLines()
+        print(self.verticalLines)
+        document.add(elements)
+
+        # document.save(pretty=True)
+        return document.tostring()
+
+    def getSVGDocument(self):
+        return Drawing(
+            'background.svg',
+            size=(str(self.width) + "mm", str(self.height) + "mm"),
+            viewBox=self.viewBox,
+            profile='tiny',
+        )
+
+    def getLines(self):
+        group = Group(**self.style)
+
+        currentY = 6
+        currentX = randint(-10 , 150)
+
+        for y in range(0, self.verticalLines):
+            while currentX < self.width:
+                randX = randint(2, 6) + currentX
+                line = Line((currentX, currentY), (randX, currentY))
+                group.add(line)
+                currentX = randX + randint(50, 150)
+            currentX = randint(-10, 150)
+            currentY = round(currentY + self.verticalInterval, 1)
+
+        return group
+
+backgroundGenerator = BackgroundGenerator()
