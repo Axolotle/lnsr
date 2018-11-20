e77842407f551c55a26280e185217953596fb00a
Axolotle <nchesnais@mailoo.org>
2018-11-20 18:28:12 +0100

    generating pdf certificate

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, Response
 from generators.backgroundGen import generate, generate_parallax
 from generators.ruler import rulerGenerator
-
+from generators.background import backgroundGenerator
+from flask_weasyprint import HTML, render_pdf
 app = Flask(__name__)
 
 
@@ -16,6 +17,20 @@ def normal():
 @app.route('/b')
 def parallax():
     return generate_parallax()
+
+@app.route('/bg')
+def bg():
+    return Response(response=backgroundGenerator.generate_file(), content_type='image/svg+xml')
+
+@app.route('/pdf')
+def pdf():
+    pdf = HTML(string=render_template('certificate.html',
+        ruler=rulerGenerator.generate_file(1346),
+        bg=backgroundGenerator.generate_file(),
+        **rulerGenerator.getContentNumbers(1346)))
+    with open('test2.pdf', 'wb') as output:
+        output.write(pdf.write_pdf())
+    return render_pdf(pdf)
 
 @app.route('/ruler')
 def rulerRoot():
