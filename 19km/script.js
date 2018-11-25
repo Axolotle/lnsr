3d516a67de820b960455dfa5b630b1a3beeb52a2
Axolotle <nchesnais@mailoo.org>
2018-11-25 16:43:59 +0100

    add methods to container array & reorganising code

--- 19km/script.js
+++ 19km/script.js
@@ -1,85 +1,60 @@
-if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
-
-var container, stats;
-var camera, scene, renderer, controls;
-
-var models, models2, models3;
-
-var clock = new THREE.Clock();
-
-var tim = Date.now();
-var distance = 0;
-var n = 3;
-
-var div = document.getElementById("data");
-var div2 = document.getElementById("dataCont");
-
-
-init(function() {
-	animate();
-});
-
-
 function init(callback) {
+	display = document.getElementById( 'display' );
 
-	container = document.getElementById( 'container' );
+	var loader = new THREE.ColladaLoader(callback);
+	loader.options.convertUpAxis = true;
+	loader.load( 'voxelContainer.dae', function ( collada ) {
+		var container = collada.scene;
+		container.scale.set(0.1, 0.1, 0.1);
 
-	//camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+		containers = [
+			[container, container.clone()],
+			[container.clone(), container.clone()],
+			[container.clone(), container.clone()]
+		]
+
+		containers.move = function (x) {
+			for (let i = 0; i < this.length; i++) {
+				for (let j = 0; j < this[i].length; j++) {
+					this[i][j].position.x += x;
+				}
+			}
+		};
+		containers.resetPosition = function () {
+			for (let i = 0; i < this[0].length; i++) {
+				this[0][i].position.x = 19;
+				this[1][i].position.x = 0;
+				this[2][i].position.x = -19;
+			}
+		};
+
+		var containersPos = [
+			[[  0, -4.1, 0], [  0, 4.1, 0]],
+			[[-19, -4.1, 0], [-19, 4.1, 0]],
+			[[-38, -4.1, 0], [-38, 4.1, 0]]
+		]
+
+		for (let i = 0; i < containers.length; i++) {
+			for (let j = 0; j < containers[i].length; j++) {
+				containers[i][j].position.set(...containersPos[i][j]);
+				scene.add(containers[i][j]);
+			}
+		}
+
+		callback();
+
+	} );
+
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
 	camera.position.set( 0, 0, -15 );
 
 	scene = new THREE.Scene();
 	camera.lookAt(scene.position);
 
-
-	// collada
-
-	var loader = new THREE.ColladaLoader(callback);
-	loader.options.convertUpAxis = true;
-	loader.load( 'voxelContainer.dae', function ( collada ) {
-		var object = collada.scene;
-
-		object.scale.set( 0.1, 0.1, 0.1);
-		object.position.set( 0, -4.1, 0 );
-
-		// for (var a = 0; a < 20; a++) {
-		// 	for (var b = 0; b < 15; b++) {
-		// 		var g = object.clone();
-		// 		g.position.y = 8.2 * b - 4.1;
-		// 		g.position.z = 7.7 * a;
-		// 		scene.add(g);
-		// 	}
-		// }
-
-		var obj2 = object.clone();
-		//x, z, y
-		obj2.position.set(0,4.1,0);
-		var obj3 = object.clone();
-		obj3.position.set(-19,-4.1,0);
-		var obj4 = obj3.clone();
-		obj4.position.set(-19,4.1,0);
-
-		var obj5 = obj2.clone();
-		obj5.position.set(-38,-4.1,0);
-		var obj6 = obj5.clone();
-		obj6.position.set(-38,4.1,0);
-
-		scene.add( object, obj2, obj3, obj4, obj5, obj6);
-		models = [object, obj2];
-		models2 = [obj3, obj4];
-		models3 = [obj5, obj6];
-
-
-		callback();
-
-	} );
-
-	//
-
-	var gridHelper = new THREE.GridHelper( 19, 20 );
-	scene.add( gridHelper );
-
-	//
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	display.appendChild( renderer.domElement );
 
 	var ambientLight = new THREE.AmbientLight( 0xFFFFFF );//0xcccccc
 	scene.add( ambientLight );
@@ -88,76 +63,61 @@ function init(callback) {
 	directionalLight.position.set( 0, 5, -1 ).normalize();
 	scene.add( directionalLight );
 
-	//
+	// var gridHelper = new THREE.GridHelper( 19, 20 );
+	// scene.add( gridHelper );
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	container.appendChild( renderer.domElement );
-
-	//
-
-	controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-	//
-
-	//stats = new Stats();
-	//container.appendChild( stats.dom );
-
-	//
+	// controls = new THREE.OrbitControls( camera, renderer.domElement );
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
 }
 
 function onWindowResize() {
-
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-
-	renderer.setSize( window.innerWidth, window.innerHeight );
-
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
 	var delta = clock.getDelta();
 
 	distance += delta*3.14159;
-	var s = Date.now();
 	let dis = (distance*10*31.8842105)/1000;
 	div2.innerHTML = "container n°" + n + "/3194"
 	div.innerHTML = (Math.round(dis*1000)/1000).toFixed(3) + " m";
 
-	if (models[0].position.x >= 38) {
-		for (var i = 0; i < models.length; i++) {
-			models[i].position.x = 19;
-			models2[i].position.x = 0;
-			models3[i].position.x = -19;
-		}
-		console.log(dis,n, dis/6.058, dis/n);
+	if (containers[0][0].position.x >= 38) {
+		containers.resetPosition();
+		console.log(dis, n, dis/6.058, dis/n);
 
 		n++;
 		tim = Date.now();
 	}
 	else {
-		for (var i = 0; i < models.length; i++) {
-			models[i].position.x += delta*3.14159;
-			models2[i].position.x += delta*3.14159;
-			models3[i].position.x += delta*3.14159;
-		}
+		containers.move(delta * 3.14159);
 	}
 
+	requestAnimationFrame(animate);
 
-	requestAnimationFrame( animate );
-
-	render();
-
-	//stats.update();
-
+	renderer.render(scene, camera);
 }
 
-function render() {
 
-	renderer.render( scene, camera );
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
-}
+var display;
+var camera, scene, renderer, controls;
+
+var containers;
+
+var clock = new THREE.Clock();
+
+var tim = Date.now();
+var distance = 0;
+var n = 1;
+
+var div = document.getElementById("data");
+var div2 = document.getElementById("dataCont");
+
+
+init(animate);
