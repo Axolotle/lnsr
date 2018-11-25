5d8480f758ab9aab3d227ba4978766491934a5c5
Axolotle <nchesnais@mailoo.org>
2018-11-25 19:12:09 +0100

    Add animation's end & reset

--- 19km/script.js
+++ 19km/script.js
@@ -1,9 +1,9 @@
 function init(callback) {
 	display = document.getElementById( 'display' );
 
-	var loader = new THREE.ColladaLoader(callback);
+	var loader = new THREE.ColladaLoader();
 	loader.options.convertUpAxis = true;
-	loader.load( 'voxelContainer.dae', function ( collada ) {
+	loader.load( 'voxelContainer.dae', async function ( collada ) {
 		var container = collada.scene;
 		container.scale.set(0.1, 0.1, 0.1);
 
@@ -11,7 +11,7 @@ function init(callback) {
 			[container, container.clone()],
 			[container.clone(), container.clone()],
 			[container.clone(), container.clone()]
-		]
+		];
 
 		containers.move = function (x) {
 			for (let i = 0; i < this.length; i++) {
@@ -20,35 +20,38 @@ function init(callback) {
 				}
 			}
 		};
-		containers.resetPosition = function () {
-			for (let i = 0; i < this[0].length; i++) {
-				this[0][i].position.x = 19;
-				this[1][i].position.x = 0;
-				this[2][i].position.x = -19;
-			}
-		};
 
-		var containersPos = [
-			[[  0, -4.1, 0], [  0, 4.1, 0]],
-			[[-19, -4.1, 0], [-19, 4.1, 0]],
-			[[-38, -4.1, 0], [-38, 4.1, 0]]
-		]
+		containers.reset = function () {
+			var containersPos = [
+				[[  0, -4.1, 0], [  0, 4.1, 0]],
+				[[-19, -4.1, 0], [-19, 4.1, 0]],
+				[[-38, -4.1, 0], [-38, 4.1, 0]]
+			]
 
-		for (let i = 0; i < containers.length; i++) {
-			for (let j = 0; j < containers[i].length; j++) {
-				containers[i][j].position.set(...containersPos[i][j]);
-				scene.add(containers[i][j]);
+			for (let i = 0; i < this.length; i++) {
+				for (let j = 0; j < this[i].length; j++) {
+					containers[i][j].position.set(...containersPos[i][j]);
+				}
 			}
 		}
 
+
+		containers.reset();
+		for (let i = 0; i < containers.length; i++) {
+			for (let j = 0; j < containers[i].length; j++) {
+				scene.add(containers[i][j]);
+			}
+		}
+		renderer.render(scene, camera);
+		await sleep(2000);
 		callback();
 
 	} );
 
+	scene = new THREE.Scene();
+
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
 	camera.position.set( 0, 0, -15 );
-
-	scene = new THREE.Scene();
 	camera.lookAt(scene.position);
 
 	renderer = new THREE.WebGLRenderer();
@@ -62,6 +65,8 @@ function init(callback) {
 	var directionalLight = new THREE.DirectionalLight( 0xffffff );
 	directionalLight.position.set( 0, 5, -1 ).normalize();
 	scene.add( directionalLight );
+
+	clock = new THREE.Clock();
 
 	// var gridHelper = new THREE.GridHelper( 19, 20 );
 	// scene.add( gridHelper );
@@ -78,30 +83,52 @@ function onWindowResize() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate() {
+async function animate() {
+	var raf = requestAnimationFrame(animate);
+	time = clock.elapsedTime;
 	var delta = clock.getDelta();
+	var actualPos = containers[0][0].position.x;
+	var step = delta * meterIn3D
 
-	distance += delta*3.14159;
-	let dis = (distance*10*31.8842105)/1000;
+	distance += step;
 	div2.innerHTML = "container n°" + n + "/3194"
-	div.innerHTML = (Math.round(dis*1000)/1000).toFixed(3) + " m";
+	div.innerHTML = (distance / meterIn3D).toFixed(3) + " m";
 
-	if (containers[0][0].position.x >= 38) {
-		containers.resetPosition();
-		console.log(dis, n, dis/6.058, dis/n);
-
+	// cancel the animation after last container
+	if (actualPos >= 57) {
+		cancelAnimationFrame(raf);
+		await sleep(2000);
+		containers.reset();
+		clock = new THREE.Clock();
+		animate();
+		distance = 0;
+		n = 1;
+	}
+	// increment container's number at first and last container
+	else if ((n === maxN - 1 && actualPos >= 38) || (n == 1 && actualPos >= 19)) {
+		console.log("distance", (distance / meterIn3D), actualPos, time);
 		n++;
-		tim = Date.now();
 	}
-	else {
-		containers.move(delta * 3.14159);
+	// reset second container's position
+	else if (n < maxN && actualPos >= 38) {
+		containers.move(-19);
+		n++;
 	}
-
-	requestAnimationFrame(animate);
+	containers.move(step);
 
 	renderer.render(scene, camera);
 }
 
+function sleep(ms) {
+	return new Promise (resolve => setTimeout(resolve, ms));
+}
+
+
+// animation speed is 1 m/s (3.6 km/h)
+// convert meter to 3D units to define 1 3Dunit/s
+var widthMeter = 6.058;
+var width3D = 19;
+var meterIn3D = width3D / widthMeter;
 
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
@@ -110,11 +137,11 @@ var camera, scene, renderer, controls;
 
 var containers;
 
-var clock = new THREE.Clock();
+var clock, time;
 
-var tim = Date.now();
 var distance = 0;
 var n = 1;
+var maxN = 4;
 
 var div = document.getElementById("data");
 var div2 = document.getElementById("dataCont");
