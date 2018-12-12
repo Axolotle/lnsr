cb98050579672d29cc0d4d4b04d5d8f3c9fbbf7e
Axolotle <nchesnais@mailoo.org>
2018-12-12 23:05:46 +0100

    zoom as a class & uniform zoom ratio & reverse zoom

--- static/js/svgDezoom.js
+++ static/js/svgDezoom.js
@@ -1,34 +1,56 @@
-function dezoom(e) {
-    let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-    let svg = document.getElementsByTagName('svg')[0];
-    zoomRatio *= 1.05;
-    let addX = delta * zoomRatio;
-    let addY = (delta * zoomRatio * -1) * 2;
-    if (x + addX < 0) {
-        x = 499.5;
-        y = 1;
-        zoomRatio = 0.1;
-        addX = addX / 1000;
-        addY = addY / 1000;
-        layers[layerNumber].classList.add('hide');
-        layers[layerNumber+1].setAttribute('transform', "matrix(0.001 0 0 0.001 499.5 499.5)")
-        layers[layerNumber+2].setAttribute('transform', "")
-        layers[layerNumber+3].classList.remove('hide');
-        layers[layerNumber+3].setAttribute('transform', "matrix(1000 0 0 1000 -499500 -499500)")
-        layerNumber++;
+class SVGMap {
+    constructor(element) {
+        this.element = element;
+        this.layers = element.querySelectorAll('#layers > g');
+        this.actualLayer = 3;
+        this.steps = 100;
+        this.step = 100;
+
+        for (let layer of this.layers) {
+            layer.classList.add('hide');
+        }
+        this.switchLayer(this.actualLayer);
+        this.zoom(0);
+
+        // IE9, Chrome, Safari, Opera
+        window.addEventListener('mousewheel', (e) => {
+            this.zoom(-Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))))
+        });
+        // Firefox
+        window.addEventListener('DOMMouseScroll', (e) => {
+            this.zoom(-Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))))
+        });
+
     }
-    x += addX;
-    y += addY;
-    console.log(x, y);
-    svg.setAttribute("viewBox", [x, x, y, y].join(' '))
+
+    zoom(delta) {
+        this.step += delta;
+        if (this.step <= 0 || this.step >= this.steps) {
+            this.step = delta === -1 ? this.steps - 1 : 0;
+            this.actualLayer += delta;
+            this.switchLayer(this.actualLayer);
+        }
+        let actualStep = this.step / this.steps;
+        let x = 499.5 - (Math.pow(actualStep, 3) * 499.5);
+        let y = Math.pow(actualStep, 3) * 999 + 1;
+        this.element.setAttribute('viewBox', [x, x, y, y].join(' '));
+
+    }
+
+    switchLayer(n) {
+        this.layers[n-2].classList.add('hide');
+        this.layers[n-1].setAttribute('transform',
+            'matrix(0.001 0 0 0.001 499.5 499.5)');
+        this.layers[n-1].classList.remove('hide');
+        this.layers[n].setAttribute('transform', '');
+        this.layers[n].classList.remove('hide');
+        this.layers[n+1].setAttribute('transform',
+            'matrix(1000 0 0 1000 -499500 -499500)');
+        this.layers[n+1].classList.remove('hide');
+        this.layers[n+2].classList.add('hide');
+        // "matrix(1000000 0 0 1000000 -499999500 -499999500)"
+
+    }
 }
 
-var zoom_lvl = 1;
-var x = 499.5;
-var y = 1;
-var zoomRatio = 0.1;
-var layers = document.querySelectorAll('svg g');
-var layerNumber = 0;
-
-window.addEventListener("mousewheel", dezoom); // IE9, Chrome, Safari, Opera
-window.addEventListener("DOMMouseScroll", dezoom); // Firefox
+var svgMap = new SVGMap(document.getElementsByTagName('svg')[0]);
