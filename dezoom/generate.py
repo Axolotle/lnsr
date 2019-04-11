3513f20eb3270bb7611e7bccea0015b02c738b20
Axolotle <nchesnais@mailoo.org>
2019-04-11 17:20:03 +0200

    add recursive generation of svg element's children and some attributes modifications

--- dezoom/generate.py
+++ dezoom/generate.py
@@ -3,14 +3,45 @@ from lxml.etree import ElementTree, Element, tostring
 from parse import yaml
 
 
-with open('dezoom.yaml', 'r') as input:
-    data = yaml.load(input)
+def itering(items, container):
+    for (tag, options) in items:
+        children = options.pop('children', None)
+        elem = Element(tag, **refactorize_options(options))
+        if children:
+            itering(children, elem)
+        container.append(elem)
 
-children = data['svg'].pop('children')
-NSMAP = data['svg'].pop('ns')
-root = Element('svg', attrib=data['svg'], nsmap=NSMAP)
-document = ElementTree(root)
+def refactorize_options(opts):
+    haddata = False
+    options = {}
+    for key, value in opts.items():
+        if key == 'data':
+            for name, val in opts[key].items():
+                options['data-' + name] = val
+                if name != 'scalename':
+                    haddata = True
+        else:
+            options[key] = str(value)
 
-with open('../templates/svgs/dezoom.svg', 'w') as output:
-    input = tostring(document, encoding='utf-8', xml_declaration=True, pretty_print=True)
-    output.write(input.decode())
+    if haddata:
+        if 'class' in options:
+            options['class'] += ' data'
+        else:
+            options['class'] = 'data'
+
+    return options
+
+if __name__ == "__main__":
+    with open('dezoom.yaml', 'r') as input:
+        data = yaml.load(input)
+
+    children = data['svg'].pop('children')
+    NSMAP = data['svg'].pop('ns')
+    root = Element('svg', attrib=data['svg'], nsmap=NSMAP)
+    document = ElementTree(root)
+
+    itering(children, root)
+
+    with open('../templates/svgs/dezoom.svg', 'w') as output:
+        input = tostring(document, encoding='utf-8', xml_declaration=True, pretty_print=True)
+        output.write(input.decode())
