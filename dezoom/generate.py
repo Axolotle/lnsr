d587c3db66135bc4780a9561e5ff6e369d75dfaa
Axolotle <nchesnais@mailoo.org>
2019-04-11 17:07:48 +0200

    generate svg with attributes and namespaces

--- dezoom/generate.py
+++ dezoom/generate.py
@@ -1,5 +1,16 @@
+from lxml.etree import ElementTree, Element, tostring
+
 from parse import yaml
+
 
 with open('dezoom.yaml', 'r') as input:
     data = yaml.load(input)
-    print(data)
+
+children = data['svg'].pop('children')
+NSMAP = data['svg'].pop('ns')
+root = Element('svg', attrib=data['svg'], nsmap=NSMAP)
+document = ElementTree(root)
+
+with open('../templates/svgs/dezoom.svg', 'w') as output:
+    input = tostring(document, encoding='utf-8', xml_declaration=True, pretty_print=True)
+    output.write(input.decode())
